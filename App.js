517e6444ac2636206fc068906ebea9a4f10537c7
Biohazard06 <jeromeweberpro@gmail.com>
2020-09-14 20:31:44 +0200

    add redux persist

--- App.js
+++ App.js
@@ -13,12 +13,18 @@ import React from 'react';
 import Navigation from './ Navigation/Navigation';
 import { Provider } from 'react-redux';
 import Store from './Store/configStore';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
 class App extends React.Component {
     render() {
+        let persistor = persistStore(Store);
+
         return (
             <Provider store={Store}>
-                <Navigation />
+                <PersistGate loading={null} persistor={persistor}>
+                    <Navigation />
+                </PersistGate>
             </Provider>
         );
     }
--- Store/configStore.js
+++ Store/configStore.js
@@ -1,7 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore } from 'redux';
 import toggleFavorite from './Reducers/favoriteReducer';
 import setAvatar from './Reducers/avatarReducer';
+import { persistCombineReducers } from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
 
 // TODO : historique de navigation (https://openclassrooms.com/fr/courses/4902061-developpez-une-application-mobile-react-native/5400081-construisez-une-action-sur-redux)
 
-export default createStore(combineReducers({toggleFavorite, setAvatar}));
+const rootPersistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+}
+
+export default createStore(persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}));
